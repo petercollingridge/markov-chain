ef828594c7140984524cbda1cb36a0c9efbcf527
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-01-15 13:29:29 +0000

    Add is_connected method

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -25,6 +25,29 @@ class MarkovChain:
         node1.edges_out.append(edge)
         node2.edges_in.append(edge)
 
+    def is_connected(self):
+        if len(self.nodes) == 0:
+            return True
+
+        visited = set()
+        frontier = set([self.nodes[0]])
+
+        while len(frontier) > 0:
+            node = frontier.pop()
+            visited.add(node)
+
+            for edge in node.edges_out:
+                next_node = edge.to_node
+                if next_node not in visited:
+                    frontier.add(next_node)
+            
+            for edge in node.edges_in:
+                next_node = edge.from_node
+                if next_node not in visited:
+                    frontier.add(next_node)
+
+        return len(visited) == len(self.nodes)
+
     def __str__(self):
         return "A Markov chain with {0} nodes and {1} edges".format(
             len(self.nodes),
@@ -41,14 +64,20 @@ class Node:
         self.edges_in = []
         self.edges_out = []
 
+    def __str__(self):
+        return "State {0}".format(self.index)
+
 
 class Edge:
     """ An edge representing the transition between two states in a Markov chain. """
 
-    def __init__(self, node1, node2, probability):
-        self.node1 = node1
-        self.node2 = node2
+    def __init__(self, from_node, to_node, probability):
+        self.from_node = from_node
+        self.to_node = to_node
         self.probability = probability
+
+    def __str__(self):
+        return "Edge from {0} to {1}".format(self.from_node.index, self.to_node.index)
 
 
 if __name__ == "__main__":
--- tests/test_chain.py
+++ tests/test_chain.py
@@ -8,3 +8,57 @@ class TestMarkovChain(unittest.TestCase):
         chain = MarkovChain()
         self.assertEqual(len(chain.nodes), 0)
         self.assertEqual(len(chain.edges), 0)
+
+    def test_add_nodes(self):
+        chain = MarkovChain()
+        chain.add_nodes(5)
+        self.assertEqual(len(chain.nodes), 5)
+        self.assertEqual(len(chain.edges), 0)
+
+
+class TestMarkovChainConnection(unittest.TestCase):
+    def test_two_circles(self):
+        chain = MarkovChain()
+        chain.add_nodes(7)
+        chain.add_edge(0, 1)
+        chain.add_edge(1, 2)
+        chain.add_edge(2, 3)
+        chain.add_edge(3, 0)
+        
+        chain.add_edge(4, 5)
+        chain.add_edge(5, 6)
+        chain.add_edge(6, 4)
+        self.assertEqual(chain.is_connected(), False)
+
+    def test_two_connected_circles(self):
+        chain = MarkovChain()
+        chain.add_nodes(7)
+        chain.add_edge(0, 1)
+        chain.add_edge(1, 2)
+        chain.add_edge(2, 3)
+        chain.add_edge(3, 0)
+        
+        chain.add_edge(4, 5)
+        chain.add_edge(5, 6)
+        chain.add_edge(6, 4)
+        chain.add_edge(4, 0)
+        self.assertEqual(chain.is_connected(), True)
+
+    def test_all_nodes_out(self):
+        chain = MarkovChain()
+        chain.add_nodes(4)
+        chain.add_edge(0, 1)
+        chain.add_edge(0, 2)
+        chain.add_edge(0, 3)
+
+        self.assertEqual(chain.is_connected(), True)
+
+    def test_all_nodes_in(self):
+        chain = MarkovChain()
+        chain.add_nodes(4)
+        chain.add_edge(1, 0)
+        chain.add_edge(2, 0)
+        chain.add_edge(3, 0)
+
+        self.assertEqual(chain.is_connected(), True)
+
