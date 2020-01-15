61e4075c83ed483d9980d7ecb62e3508e3759b26
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-01-15 14:46:18 +0000

    Tests for adding edges

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -4,16 +4,51 @@ from src.markov_chain import MarkovChain
 
 
 class TestMarkovChain(unittest.TestCase):
-    def test_create_chain(self):
+    def test_create_empty_chain(self):
         chain = MarkovChain()
         self.assertEqual(len(chain.nodes), 0)
         self.assertEqual(len(chain.edges), 0)
 
+    def test_create_chain_with_nodes(self):
+        chain = MarkovChain(5)
+        self.assertEqual(len(chain.nodes), 5)
+        self.assertEqual(len(chain.edges), 0)
+
+    def test_add_node(self):
+        chain = MarkovChain()
+        chain.add_node()
+        chain.add_node()
+
+        self.assertEqual(len(chain.nodes), 2)
+        self.assertEqual(chain.nodes[0].index, 0)
+        self.assertEqual(chain.nodes[1].index, 1)
+
     def test_add_nodes(self):
         chain = MarkovChain()
         chain.add_nodes(5)
+
         self.assertEqual(len(chain.nodes), 5)
         self.assertEqual(len(chain.edges), 0)
+
+    def test_add_edge(self):
+        chain = MarkovChain(3)
+        chain.add_edge(0, 1)
+        chain.add_edge(1, 2, 0.7)
+        chain.add_edge(1, 2, 0.25)
+
+        self.assertEqual(len(chain.nodes), 3)
+        self.assertEqual(len(chain.edges), 3)
+        self.assertEqual(len(chain.nodes[1].edges_out), 2)
+        self.assertEqual(chain.nodes[1].edges_out[1].probability, 0.25)
+
+    def test_add_edges(self):
+        chain = MarkovChain(3)
+        chain.add_edges(((0, 1), (1, 2, 0.75), (1, 0, 0.25)))
+
+        self.assertEqual(len(chain.nodes), 3)
+        self.assertEqual(len(chain.edges), 3)
+        self.assertEqual(len(chain.nodes[1].edges_out), 2)
+        self.assertEqual(chain.nodes[1].edges_out[1].probability, 0.25)
 
 
 class TestMarkovChainConnection(unittest.TestCase):
