70d4ec1b8ffa392febddee10e70f8e99d4609e69
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-01-17 16:35:49 +0000

    Pad matrix for nice display

--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,13 @@ from fractions import Fraction
 
 
 def write_matrix_with_fractions(matrix):
+    # Get 2D list of strings showing fractions
+    strings = list()
     for row in matrix:
-        print(", ".join(str(Fraction(item).limit_denominator()) for item in row))
+        strings.append([str(Fraction(value).limit_denominator()) for value in row])
+
+    # Pad with two more spaces than the longest string
+    padding = max(len(value) for row in strings for value in row)
+
+    for row in strings:
+        print("  ".join("{value: <{padding}}".format(value=value, padding=padding) for value in row))
